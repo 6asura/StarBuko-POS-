71fe5e4b55420b4c82c6aa27e627015b7ddb9d89
jemri <jemri@ASURA>
2026-04-21 09:32:16 +0800

    Safety checks

--- DbHelper.cs
+++ DbHelper.cs
@@ -1,16 +1,38 @@
 ﻿using MySql.Data.MySqlClient;
+using System;
 using System.Configuration;
+using System.IO;
+using System.Xml;
 
 namespace Starbuko
 {
     public static class DbHelper
     {
-        private static readonly string connectionString =
-            ConfigurationManager.ConnectionStrings["MyDb"].ConnectionString;
-
         public static MySqlConnection GetConnection()
         {
+            string connectionString = GetConnectionString();
             return new MySqlConnection(connectionString);
+        }
+
+        private static string GetConnectionString()
+        {
+            string localConfigPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\App.config.local");
+            localConfigPath = Path.GetFullPath(localConfigPath);
+
+            if (File.Exists(localConfigPath))
+            {
+                XmlDocument xml = new XmlDocument();
+                xml.Load(localConfigPath);
+
+                XmlNode node = xml.SelectSingleNode("//connectionStrings/add[@name='MyDb']");
+
+                if (node != null && node.Attributes["connectionString"] != null)
+                {
+                    return node.Attributes["connectionString"].Value;
+                }
+            }
+
+            return ConfigurationManager.ConnectionStrings["MyDb"].ConnectionString;
         }
     }
 }
